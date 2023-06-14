56c78920a6849756811e5f7ed2ccc0a69cf6373f
Harry262000 <86480339+Harry262000@users.noreply.github.com>
2023-06-14 19:41:45 +0530

    commit

--- docs/static/style.js
+++ docs/static/style.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $(".content").slice(0, 2).show();
+    $(".content").slice(0, 4).show();
     $("#loadMore").on("click", function(e) {
         e.preventDefault();
-        $(".content:hidden").slice(0, 2).slideDown();
+        $(".content:hidden").slice(0, 4).slideDown();
         if ($(".content:hidden").length == 0) {
             $("#loadMore").text("No Content").addClass("noContent");
         }
